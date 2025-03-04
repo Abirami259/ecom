ebc591ebeb961d10330e1b9203ec22a8a3e672eb
Abirami259 <abirami3702@gmail.com>
2025-03-04 11:48:05 +0530

    initial commit

---
+++ src/main/java/com/ecom/customer/order/model/Customer.java
@@ -0,0 +1,32 @@
+package com.ecom.customer.order.model;
+
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.Order;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Order> orders;
+}
+
