524e30d3fa998629fbbdd9ac7919a1750d893c84
Abirami259 <abirami3702@gmail.com>
2025-03-11 10:54:38 +0530

    second commit

---
+++ src/main/java/com/ecom/customer/order/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.ecom.customer.order.repository;
+
+
+import com.ecom.customer.order.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> { }
